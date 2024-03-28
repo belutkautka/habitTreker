ea39fabcb0df0111970808ad1a8533c8afad6d44
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-03-28 19:28:36 +0500

    patch 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,3 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-
-dependencies {
-    classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5"
-}
