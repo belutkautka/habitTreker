a3d1a0941c6aad09d944076519063c0130235cbb
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-04-09 13:53:19 +0500

    Рефакторинг

--- app/src/main/java/com/application/hw2/db/HabitsList.kt
+++ app/src/main/java/com/application/hw2/db/HabitsList.kt
@@ -1,36 +1,23 @@
 package com.application.hw2.db
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.application.hw2.enums.HabitType
 import com.application.hw2.model.HabitModel
 
 // Это симуляция бд
 object HabitsList {
-    private val _habits = MutableLiveData<MutableList<HabitModel>>()
-
-    // LiveData для наблюдения за изменениями
-    val habits: LiveData<MutableList<HabitModel>> = _habits
-
-    init {
-        _habits.value = mutableListOf()
-    }
+    val habits: MutableList<HabitModel> = mutableListOf()
 
     fun selectHabitsByType(type: HabitType): List<HabitModel> {
-        return _habits.value!!.filter { habit -> habit.type == type }
+        return habits.filter { habit -> habit.type == type }
     }
 
     fun updateHabit(oldHabit: HabitModel, newHabit: HabitModel) {
-        val index = _habits.value!!.indexOf(oldHabit)
-        val updatedHabits = _habits.value!!.toMutableList()
-        updatedHabits.removeAt(index)
-        updatedHabits.add(newHabit)
-        _habits.value = updatedHabits
+        val index = habits.indexOf(oldHabit)
+        habits.removeAt(index)
+        habits.add(newHabit)
     }
 
     fun insertToEnd(habit: HabitModel) {
-        val updatedHabits = _habits.value!!.toMutableList()
-        updatedHabits.add(habit)
-        _habits.value = updatedHabits
+        habits.add(habit)
     }
 }
--- app/src/main/java/com/application/hw2/viewModels/MainVM.kt
+++ app/src/main/java/com/application/hw2/viewModels/MainVM.kt
@@ -1,13 +1,21 @@
 package com.application.hw2.viewModels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.application.hw2.db.HabitsList
 import com.application.hw2.enums.HabitType
 import com.application.hw2.model.HabitModel
 
 class MainVM : ViewModel() {
-    val habits: LiveData<MutableList<HabitModel>> = HabitsList.habits
+    private val _habits = MutableLiveData<MutableList<HabitModel>>()
+
+    // LiveData для наблюдения за изменениями
+    val habits: LiveData<MutableList<HabitModel>> = _habits
+
+    init {
+        _habits.value = HabitsList.habits
+    }
 
     fun addHabit(newHabit: HabitModel) {
         HabitsList.insertToEnd(newHabit)
