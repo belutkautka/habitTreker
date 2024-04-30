1039b60dfdeb69975e66d68fe096cb727bcda146
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-04-30 12:48:08 +0500

    fix

--- app/src/main/java/com/application/hw2/db/HabitRepository.kt
+++ app/src/main/java/com/application/hw2/db/HabitRepository.kt
@@ -4,15 +4,13 @@ import androidx.lifecycle.LiveData
 import com.application.hw2.model.HabitModel
 
 class HabitRepository(private val habitDao: HabitsDao) {
-    var allHabits: LiveData<List<HabitModel>> = habitDao.selectAllHabits()
+    val allHabits: LiveData<List<HabitModel>> = habitDao.selectAllHabits()
 
     suspend fun insert(habit: HabitModel) {
         habitDao.insertHabit(habit)
-        allHabits = habitDao.selectAllHabits()
     }
 
     suspend fun update(habit: HabitModel) {
         habitDao.updateHabit(habit)
-        allHabits = habitDao.selectAllHabits()
     }
 }
--- app/src/main/java/com/application/hw2/viewModels/FilterVM.kt
+++ app/src/main/java/com/application/hw2/viewModels/FilterVM.kt
@@ -3,44 +3,37 @@ package com.application.hw2.viewModels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.MediatorLiveData
 import com.application.hw2.db.AppDatabase
 import com.application.hw2.db.HabitRepository
 import com.application.hw2.model.HabitModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class FilterVM(application: Application) : AndroidViewModel(application) {
     private val appDatabase = AppDatabase.getHabitsDatabase(getApplication())
     private val habitsDao = appDatabase.habitsDao()
     private val repository: HabitRepository = HabitRepository(habitsDao)
 
-//    private var _habits = MutableLiveData<List<HabitModel>>()
-
-    lateinit var habits: LiveData<List<HabitModel>>
-
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            habits = repository.allHabits
-        }
+    private val _habits = MediatorLiveData<List<HabitModel>>().apply {
+        addSource(repository.allHabits) { value = it }
     }
+    val habits: LiveData<List<HabitModel>> = _habits
 
     fun sortByPriority(desc: Boolean) {
-        if (habits.value!!.isEmpty())
+        if (_habits.value!!.isEmpty())
             return
         if (!desc) {
             val sortedList = repository.allHabits.value?.sortedBy { it.name }
 
-            habits = MutableLiveData(sortedList)
+            _habits.value = sortedList
         } else {
             val sortedList = repository.allHabits.value?.sortedByDescending { it.name }
-            habits = MutableLiveData(sortedList)
+            _habits.value = sortedList
         }
     }
 
     fun searchHabits(name: String) {
-        val filteredList = repository.allHabits.value?.filter { it.name.contains(name, ignoreCase = true) }
-        habits = MutableLiveData(filteredList)
+        val filteredList =
+            repository.allHabits.value?.filter { it.name.contains(name, ignoreCase = true) }
+        _habits.value = filteredList
     }
 }
