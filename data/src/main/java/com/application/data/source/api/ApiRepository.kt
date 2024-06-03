e62a41d734de57269709aaaf56b7625a01b5978b
an.s.belousova <an.s.belousova@tinkoff.ru>
2024-06-03 14:12:20 +0500

    фикс

--- data/src/main/java/com/application/data/source/api/ApiRepository.kt
+++ data/src/main/java/com/application/data/source/api/ApiRepository.kt
@@ -4,8 +4,9 @@ import com.application.data.restful.ApiService
 import com.application.domain.useCases.model.HabitFromServer
 import com.application.domain.useCases.model.RequestDone
 import com.application.domain.useCases.model.ResponseUid
+import javax.inject.Inject
 
-open class ApiRepository(private val api: ApiService){
+open class ApiRepository @Inject constructor(private val api: ApiService){
     suspend fun getHabits(): List<HabitFromServer> = api.getHabits()
     suspend fun  postHabitsDone(done: RequestDone) = api.postHabitDone(done)
     suspend fun putHabit(habit: HabitFromServer) = api.putHabit(habit)
--- data/src/main/java/com/application/data/source/database/DbRepository.kt
+++ data/src/main/java/com/application/data/source/database/DbRepository.kt
@@ -1,9 +1,10 @@
 package com.application.data.source.database
 
 import com.application.domain.useCases.model.HabitModel
+import javax.inject.Inject
 
 
-open class DbRepository(private val dao: HabitsDao) {
+open class DbRepository @Inject constructor(private val dao: HabitsDao) {
     suspend fun insertHabit(habit: HabitModel) = dao.insertHabit(habit)
     suspend fun getAllHabits(): List<HabitModel> = dao.getAllHabits()
     suspend fun deleteHabit(habit: HabitModel) = dao.deleteHabit(habit)
